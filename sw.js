f559e7af80dcfc818a4133ad580cc3105fbe8996
HardikStx <149145283+HardikStx@users.noreply.github.com>
2024-03-23 18:01:40 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,21 @@
 const CACHE_NAME = 'name-entry-app-cache-v1';
-// Use the install event to pre-cache all initial resources.
-self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
+const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon512_maskable.png'
-   '/icon512_rounded.png'
-  ]);
-  })());
+  '/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
-
-    // Get the resource from the cache.
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) {
-      return cachedResponse;
-    } else {
-        try {
-          // If the resource was not in the cache, try the network.
-          const fetchResponse = await fetch(event.request);
-    
-          // Save the resource in the cache and return it.
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        } catch (e) {
-          // The network failed.
-        }
-    }
-  })());
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
