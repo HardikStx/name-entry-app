b345501a4502ac6fa3d9f133b5a078b589ab4715
HardikStx <149145283+HardikStx@users.noreply.github.com>
2024-03-23 17:24:41 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'name-entry-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png' // Make sure icon.png is in the same directory as your other files
+  // Add any additional files or assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
